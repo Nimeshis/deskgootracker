c903f68d38bc438951eede858176b6087a76e04b
Nimeshis <dhungelnimesh5@gmail.com>
2025-01-03 17:54:31 +0545

    authenticate token

--- middleware/authenticationToken.js
+++ middleware/authenticationToken.js
@@ -1,50 +1,24 @@
 const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const accessToken = authHeader && authHeader.split(" ")[1];
-  const refreshToken = req.headers["x-refresh-token"];
+  const token = req.headers["authorization"] || req.headers["Authorization"];
 
-  if (!accessToken) {
-    return res.status(401).json({ message: "Access token required" });
+  if (!token.startsWith("Bearer")) {
+    return res.status(400).json({ error: "Invalid token format" });
+  }
+  const accessToken = token.split(" ")[1];
+
+  if (!token) {
+    return res.status(400).json({ error: "No token provided" });
   }
 
-  jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (!err) {
-      req.user = user;
-      return next();
+  jwt.verify(accessToken, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(401).json({ error: "Invalid or expired token" });
     }
 
-    if (err.name === "TokenExpiredError") {
-      // Access token has expired, try to use the refresh token
-      if (!refreshToken) {
-        return res.status(401).json({ message: "Refresh token required" });
-      }
-
-      // Verify the refresh token
-      jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET,
-        (err, user) => {
-          if (err) {
-            return res.status(403).json({ message: "Invalid refresh token" });
-          }
-
-          // Generate a new access token using the refresh token
-          const newAccessToken = jwt.sign(
-            { id: user.id },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: "15m" }
-          );
-          res.setHeader("x-access-token", newAccessToken);
-
-          req.user = user;
-          return next();
-        }
-      );
-    } else {
-      return res.status(403).json({ message: "Invalid access token" });
-    }
+    req.user = user;
+    next();
   });
 };
 
