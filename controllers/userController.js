4865257da06a97ed7bd275e0f03cdf5e88837626
Nimesh <dhungelnimesh5@gmail.com>
2025-01-06 15:45:33 +0545

    login username field

--- controllers/userController.js
+++ controllers/userController.js
@@ -138,7 +138,7 @@ const loginUser = async (req, res) => {
       refreshToken,
       user: {
         userId: user._id,
-        username: user.username,
+        username: user.fullName,
         email: user.email,
         role: user.role,
         firstLogin: user.firstLogin,
